5af7849870c865a9d21fec0adf4d61695ed319aa
Brendon Richards <brendonrichards93@gmail.com>
2018-08-20 11:44:32 +0000

    Install chalk, debug & morgan

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+// Requirements
 const express = require("express"),
+      chalk = require("morgan"),
+      debug = require("morgan"),
+      morgan = require("morgan"),
       port = process.env.PORT;
       
+// Initialize app
 const app = express();
 
+// Use Morgan
+app.use(morgan("tiny"));
+
+// Index route
 app.get("/", (req, res) => {
   res.send("Application working");
 });
 
+// 
 app.listen(port, (req, res) => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${chalk.green(port)}`);
 });
