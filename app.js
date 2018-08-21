1ac6b9ba061bb2cdb8db90e1a98457fecc795c76
Brendon Richards <brendonrichards93@gmail.com>
2018-08-21 08:29:35 +0000

    Install eslint & refactor app.js

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 // Requirements
-const express = require("express"),
-      chalk = require("chalk"),
-      debug = require("debug"),
-      morgan = require("morgan"),
-      path = require("path"),
-      port = process.env.PORT;
-      
+const express = require('express');
+
+
+const chalk = require('chalk');
+
+
+const debug = require('debug');
+
+
+const morgan = require('morgan');
+
+
+const path = require('path');
+
+
+const port = process.env.PORT;
+
 // Initialize app
 const app = express();
 
 // Use Morgan
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // Use public files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Use Semantic UI CSS
-app.use("/css", express.static(path.join(__dirname, "/semantic/dist/")));
+app.use('/css', express.static(path.join(__dirname, '/semantic/dist/')));
 
 // Use Semantic UI JS
-app.use("/js", express.static(path.join(__dirname, "/semantic/dist/")));
+app.use('/js', express.static(path.join(__dirname, '/semantic/dist/')));
 
 // Use jQuery
-app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist/")));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
 // Index route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-// 
+//
 app.listen(port, (req, res) => {
   debug(`Server started on port ${chalk.green(port)}...`);
 });
